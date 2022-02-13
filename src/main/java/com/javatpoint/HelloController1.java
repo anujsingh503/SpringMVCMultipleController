276b73b04ffdfe87e039cb4db71a79528c5ed14d
anujsingh503 <89093403+anujsingh503@users.noreply.github.com>
2022-02-13 22:54:52 +0530

    second commit
    Hi

--- src/main/java/com/javatpoint/HelloController1.java
+++ src/main/java/com/javatpoint/HelloController1.java
@@ -2,6 +2,7 @@ package com.javatpoint;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 //Ram Singh
+//Anuj Singh
 @Controller
 public class HelloController1 {
 @RequestMapping("/hello1")
